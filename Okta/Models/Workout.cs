14d6bc5e8119b06f8df8593e7670ba78b4e0a00f
Kayoung <tat.tot.tat@gmail.com>
2020-02-27 11:05:50 +1100

    Add project files.

---
+++ Okta/Models/Workout.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Okta.Models
+{
+    public class Workout
+    {
+        public int Id { get; set; }
+
+        public DateTimeOffset Date { get; set; }
+
+        public int DistanceInMeters { get; set; }
+
+        public long TimeInSeconds { get; set; }
+    }
+}
